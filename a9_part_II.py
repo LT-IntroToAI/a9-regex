ddf5f126178ab50ac2877c33dde8698e0b86b5d5
Robert Berg <rberg@cps.edu>
2025-05-08 14:55:39 -0500

    Fixed the asserts for part II

--- a9_part_II.py
+++ a9_part_II.py
@@ -60,8 +60,8 @@ if __name__ == "__main__":
     # print('\n<<<< Running asserts, this might take a sec >>>>')
     # assert get_planet_radius("Mars") == "3376.2", "Incorrect radius for Mars"
     # assert get_planet_radius("Earth") == "6356.752", "Incorrect radius for Earth"
-    # assert get_planet_radius("Jupiter") == "66,854", "Incorrect radius for Jupiter"
-    # assert get_planet_radius("Saturn") == "54,364", "Incorrect radius for Saturn"
+    # assert get_planet_radius("Jupiter") == "66854", "Incorrect radius for Jupiter"
+    # assert get_planet_radius("Saturn") == "54364", "Incorrect radius for Saturn"
     # print('\n<<<< Planet radius tests passed >>>>')
 
     print("\n<<<<<<<<<<<<<< Testing Birth Dates >>>>>>>>>>>>>>")
